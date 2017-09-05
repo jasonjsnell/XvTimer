408fbe902e61cadb966bf11a0266353454c0f12d
jasonjsnell <snell.jason@gmail>
2017-09-05 16:11:02 -0500

    Initial Commit

---
+++ XvTimer/XvTimer.h
@@ -0,0 +1,19 @@
+//
+//  XvTimer.h
+//  XvTimer
+//
+//  Created by Jason Snell on 9/5/17.
+//  Copyright © 2017 Jason J. Snell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for XvTimer.
+FOUNDATION_EXPORT double XvTimerVersionNumber;
+
+//! Project version string for XvTimer.
+FOUNDATION_EXPORT const unsigned char XvTimerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XvTimer/PublicHeader.h>
+
+
